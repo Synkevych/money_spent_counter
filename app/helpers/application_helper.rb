dd0649cbf4c006bcf2928f222f634f0649038adf
Synkevych <sunkevu4@gmail.com>
2020-11-27 15:52:28 +0200

    feat: add copy link func, refactor app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def get_pages_info(current_page, total_pages)
-    string = "Showing "
-    if current_page > 1
-      string += (current_page*10-9).to_s
-    else 
-      string << "1"
-    end
-    string << " to " << (current_page*10).to_s << " of " << total_pages.to_s << " entries"
-  end
   def get_row_number(page, i)
     i = i + 1
     if page >= 1
